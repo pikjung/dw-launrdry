d57cbd78ca402e142198058c3ea1d1ca1747240d
fikritanjung <tanjungfikri1@gmail.com>
2021-01-27 15:45:59 +0700

    Added schema for transaction table

--- database/migrations/2021_01_27_042049_create_transactions_table.php
+++ database/migrations/2021_01_27_042049_create_transactions_table.php
@@ -15,7 +15,18 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('courier_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('amount');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('status')->default(false);
             $table->timestamps();
+
+            $table->foreign('courier__id')->references('id')->on('users');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
