ee8bd1929e4d675f06f1a60755c92a1fd08a67be
fikritanjung <tanjungfikri1@gmail.com>
2021-01-28 15:16:05 +0700

    Added markup to create a SPA

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laundry Apps</title>
+
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="{{asset('/bower_components/bootstrap/dist/css/bootstrap.min.csss')}}">
+    <link rel="stylesheet" href="{{asset('/bower_components/font-awesome/dist/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/bower_components/Ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/css/AdminLTE.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/skins/_all-skins.min.csss')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+</head>
+<body class="hold-transition skin-blue layout-top-nav">
+    <div id="dw">
+        <app></app>
+    </div>    
+
+    <script src="{{asset('/bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{asset('/bower_components/fastclick/lib/fastclick.js')}}"></script>
+    <script src="{{asset('/assets/js/adminlte.min.js')}}"></script>
+    <script src="{{asset('/assets/js/demo.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
