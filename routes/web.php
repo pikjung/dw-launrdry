c87a9a1e93aadf9a833047cbdd226fbbfba23b6c
fikritanjung <tanjungfikri1@gmail.com>
2021-01-28 15:22:34 +0700

    Changed route in routes/web.php contains any routes direct to FrontController index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Models\FrontController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', [FrontController::class, 'index']);
